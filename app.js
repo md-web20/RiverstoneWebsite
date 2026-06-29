a2d8f8817005f16c299327b97bbf8d0b2ff40f90
Web-APP <cpamarketig420@gmail.com>
2026-06-30 01:47:56 +0600

    Web Service

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const opener = require('opener');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { title: "Riverstone Games | Premium Digital Studio" });
+});
+
+app.get('/games', (req, res) => res.render('games', { title: "Games - Riverstone" }));
+app.get('/web', (req, res) => res.render('web', { title: "Web Development - Riverstone" }));
+app.get('/apps', (req, res) => res.render('apps', { title: "App Development - Riverstone" }));
+app.get('/portfolio', (req, res) => res.render('portfolio', { title: "Our Portfolio" }));
+app.get('/contact', (req, res) => res.render('contact', { title: "Contact Us" }));
+app.get('/about', (req, res) => res.render('about', { title: "About Us | Riverstone Games" }));
+app.get('/privacy', (req, res) => res.render('privacy', { title: "Privacy Policy | Riverstone Games" }));
+app.get('/terms', (req, res) => res.render('terms', { title: "Terms & Conditions | Riverstone Games" }));
+app.get('/signin', (req, res) => {res.render('signin', { title: "Sign In | Riverstone Games" });});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+    opener(`http://localhost:${PORT}`);
+});
